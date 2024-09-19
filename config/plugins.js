10730bea78f0ff31c984c640be29a7cc8b0a86cb
Normen Knöbel <kontakt@taktart-design.de>
2024-09-19 22:19:14 +0200

    add plugin response transformer

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = ({env}) => ({
+
+  'transformer': {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    }
+  }
+});
