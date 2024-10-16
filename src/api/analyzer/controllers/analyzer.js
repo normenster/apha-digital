4b36f5f73cfff5b4138839b459cde6b3ecccc25a
Normen Knöbel <kontakt@taktart-design.de>
2024-10-16 10:17:45 +0200

    add query params to analyzer

--- src/api/analyzer/controllers/analyzer.js
+++ src/api/analyzer/controllers/analyzer.js
@@ -5,17 +5,20 @@ module.exports = {
     try {
       // Capture the data sent to Strapi's endpoint
       const receivedData = ctx.request.body;
+      const queryParams = ctx.query;
+
 
       // Example of forwarding data to another server
-      const response = await axios.post('http://aphadigital.th-wildau.de:9000/asr/pipeline', receivedData);
+      const response = await axios.post('http://aphadigital.th-wildau.de:9000/asr/pipeline', receivedData, {params: queryParams});
 
       // Return the response from the other server
       ctx.send({
-        message: 'Data forwarded successfully',
-        response: response.data
+        message: 'Data forwarded successfully', response: response.data
       });
     } catch (error) {
-      ctx.throw(500, 'An error occurred while forwarding the data');
+
+      console.log("#error", error);
+      ctx.throw(500, error.message);
     }
   }
 };
